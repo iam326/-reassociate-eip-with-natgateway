0f8679b7ba9be7e356baa7abd7e57cebdfec934c
takahashi.tatsunari <takahashi.tatsunari@classmethod.jp>
2021-01-24 18:23:05 +0900

    impl

--- lib/reassociate-eip-with-natgateway-stack.ts
+++ lib/reassociate-eip-with-natgateway-stack.ts
@@ -1,9 +1,35 @@
 import * as cdk from '@aws-cdk/core';
+import { CfnNatGateway, Vpc, SubnetType } from '@aws-cdk/aws-ec2';
 
 export class ReassociateEipWithNatgatewayStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new Vpc(this, 'Vpc', {
+      cidr: '10.0.0.0/16',
+      enableDnsHostnames: true,
+      enableDnsSupport: true,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'PublicSubnet',
+          subnetType: SubnetType.PUBLIC,
+        },
+      ],
+      natGateways: 2,
+      maxAzs: 2,
+    });
+
+    const allocationIds: string[] = this.node.tryGetContext('eipAllocationIds');
+    vpc.publicSubnets.forEach((subnet, index) => {
+      // 各 Subnet から NAT Gateway を探す
+      const natGateway = subnet.node.children.find(
+        (child) => child.node.id == 'NATGateway'
+      ) as CfnNatGateway;
+      // 自動で作成された EIP を削除する
+      subnet.node.tryRemoveChild('EIP');
+      // 事前に作成した EIP を設定する
+      natGateway.allocationId = allocationIds[index];
+    });
   }
 }
